6a6574fa27c4001310b039d9a5bfcb4e627a51c9
Benjie Gillam <benjie@jemjie.com>
2018-10-27 14:51:43 +0100

    Start client too

--- client/.babelrc.js
+++ client/.babelrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   presets: [
     [
+      "@babel/preset-react",
+      {
+        development: process.env.NODE_ENV === "development",
+      },
+    ],
+    [
       "@babel/env",
       {
         targets: {
