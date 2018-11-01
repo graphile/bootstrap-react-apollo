624716323549534d254a8c5515467db7a2c3afb8
Benjie Gillam <benjie@jemjie.com>
2018-11-01 15:20:30 +0000

    Support multiple lines in migrations.

--- db/scripts/create-migration.js
+++ db/scripts/create-migration.js
@@ -4,22 +4,34 @@
  *
  * https://db-migrate.readthedocs.io/en/latest/Getting%20Started/usage/#using-files-for-sqls
  */
-const readline = require('readline');
+/* eslint-disable no-console */
+const readline = require("readline");
 const fs = require("fs");
 const { dirname, relative } = require("path");
 
-
 const root = dirname(__dirname);
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const ask = msg => new Promise(resolve => rl.question(msg + '\n', answer => {console.log(); resolve(answer);}));
+const ask = (msg, multiline = false) => {
+  const allData = [];
+  let lastResponse;
+  do {
+    lastResponse = new Promise(resolve =>
+      rl.question(`${msg}\n`, answer => {
+        resolve(answer);
+      })
+    );
+    allData.push(lastResponse);
+  } while (multiline && lastResponse);
+  console.log();
+  return allData.join("\n") + (multiline ? "\n" : "");
+};
 
 async function main() {
-
   /* Migration name */
   const name = process.argv[2];
   const NAME_REGEX = /^[a-z][a-z0-9-_.]*$/;
@@ -36,7 +48,9 @@ async function main() {
     process.exit(1);
   }
   const allMigrations = fs.readdirSync(`${root}/migrations/.db-migrate`);
-  const migrationWithSameName = allMigrations.find(n => n.substr(15) === `${name}.js`);
+  const migrationWithSameName = allMigrations.find(
+    n => n.substr(15) === `${name}.js`
+  );
   if (migrationWithSameName) {
     console.error(
       `Migration name is already in use: ${migrationWithSameName}, please pick a different migration name`
@@ -47,26 +61,35 @@ async function main() {
   /* Migration timestamp */
   const pad = i => (i < 10 ? `0${i}` : String(i));
   const date = new Date();
-  const datestamp =
-    `${date.getUTCFullYear()}_${pad(date.getUTCMonth() + 1)}_${pad(
-      date.getUTCDate()
-    )}__${pad(date.getUTCHours())}_${pad(date.getUTCMinutes())}_${pad(
-      date.getUTCSeconds()
-    )}`;
-  const compressedDatestamp = datestamp.replace(/_/g, '');
+  const datestamp = `${date.getUTCFullYear()}${pad(
+    date.getUTCMonth() + 1
+  )}${pad(date.getUTCDate())}_${pad(date.getUTCHours())}${pad(
+    date.getUTCMinutes()
+  )}${pad(date.getUTCSeconds())}`;
+  const compressedDatestamp = datestamp.replace(/_/g, "");
 
   /* Leg up on migration creation */
-  const upContent = await ask('Up migration content? (Press enter to skip and add this later.)');
-  const downContent = upContent ? await ask('Down migration content? (Press enter to skip and add this later.)') : '';
+  const upContent = await ask(
+    "Up migration content? (Add a blank line to exit this)",
+    true
+  );
+  const downContent = upContent
+    ? await ask("Down migration content? (Add a blank line to exit this)", true)
+    : "";
 
   /* Create migration files */
-  const prefix = `${root}/migrations/${datestamp}-${name}`;
-  const boilerplateMigrationPath = `${root}/migrations/.db-migrate/${compressedDatestamp}-${name.replace(/\./g, '__')}.js`;
+  const prefix = `${root}/migrations/${datestamp}.${name}`;
+  const boilerplateMigrationPath = `${root}/migrations/.db-migrate/${compressedDatestamp}-${name.replace(
+    /\./g,
+    "__"
+  )}.js`;
 
   fs.writeFileSync(
     boilerplateMigrationPath,
     `/* This is boilerplate, you want the parent directory for the actual migrations */
-module.exports = require('../../scripts/migrate')(${JSON.stringify(`${datestamp}-${name}`)});\n`
+module.exports = require('../../scripts/migrate')(${JSON.stringify(
+      `${datestamp}-${name}`
+    )});\n`
   );
 
   fs.writeFileSync(`${prefix}-up.sql`, upContent);
@@ -74,19 +97,29 @@ module.exports = require('../../scripts/migrate')(${JSON.stringify(`${datestamp}
 
   /* Helpful(ish) output */
   const rel = filepath => relative(`${root}/..`, filepath);
-  console.log("Created these up and down migrations for you, please go edit them:");
+  console.log(
+    "Created these up and down migrations for you, please go edit them:"
+  );
   console.log();
   console.log(`  ${rel(`${prefix}-up.sql`)}`);
   console.log(`  ${rel(`${prefix}-down.sql`)}`);
   console.log();
-  console.log("If you messed up the name and haven't ran migrated yet, you can remove them relevant files with this command:");
+  console.log(
+    "If you messed up the name and haven't ran migrated yet, you can remove them relevant files with this command:"
+  );
   console.log();
-  console.log(`  rm "${rel(`${prefix}-up.sql`)}" "${rel(`${prefix}-down.sql`)}" "${rel(`${boilerplateMigrationPath}`)}"`);
+  console.log(
+    `  rm "${rel(`${prefix}-up.sql`)}" "${rel(`${prefix}-down.sql`)}" "${rel(
+      `${boilerplateMigrationPath}`
+    )}"`
+  );
 }
 
-main().catch(e => {
-  console.error(e);
-  process.exit(5);
-}).finally(() => {
-  rl.close();
-});
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(5);
+  })
+  .finally(() => {
+    rl.close();
+  });
