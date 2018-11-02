45a95405991915e354c660dfa0fc94d2180780c5
Benjie Gillam <benjie@jemjie.com>
2018-11-02 18:55:25 +0000

    Existing jobs code

---
+++ db/migrations/.db-migrate/20181102183925-app_jobs__schema__create.js
@@ -0,0 +1,2 @@
+/* This is boilerplate, you want the parent directory for the actual migrations */
+module.exports = require('../../scripts/migrate')("20181102_183925.app_jobs.schema.create");
---
+++ db/migrations/20181102_183925.app_jobs.schema.create-down.sql
@@ -0,0 +1,1 @@
+DROP SCHEMA app_jobs CASCADE;
