31de41472b3ecb296157b347b86dd849b007f8af
Benjie Gillam <benjie@jemjie.com>
2018-12-05 21:03:56 +0000

    Add comments

--- server/index.js
+++ server/index.js
@@ -11,28 +11,46 @@ const PORT = parseInt(process.env.PORT, 10) || 3000;
 const isDev = process.env.NODE_ENV === "development";
 
 async function main() {
-  // Our Express server
+  /*
+   * Our Express server
+   */
   const app = express();
 
-  // Getting access to the HTTP server directly means that we can do things
-  // with websockets if we need to.
+  /*
+   * Getting access to the HTTP server directly means that we can do things
+   * with websockets if we need to (e.g. GraphQL subscriptions).
+   */
   const httpServer = createServer(app);
   app.set("httpServer", httpServer);
-  // When we're using websockets, we may want them to have access to sessions/etc
+
+  /*
+   * When we're using websockets, we may want them to have access to
+   * sessions/etc for authentication.
+   */
   const socketMiddlewares = [];
   app.set("socketMiddlewares", socketMiddlewares);
 
+  /*
+   * Middleware is installed from the /server/middleware directory. These
+   * helpers may augment the express app with new settings and/or install
+   * express middleware. These helpers may be asynchronous, but they should
+   * operate very rapidly to enable quick as possible server startup.
+   */
   await middleware.installDatabasePools(app);
   if (isDev) {
-    // We're using a non-super-user connection string (authPgPool), so we need to
-    // install the watch fixtures ourselves.
+    /*
+     * We're using a non-super-user connection string (authPgPool), so we need
+     * to install the watch fixtures ourselves.
+     */
     await middleware.installDatabaseWatchFixtures(app);
   }
   await middleware.installSession(app);
   await middleware.installPassport(app);
   await middleware.installLogging(app);
+  // These are our assets: images/etc; served out of the /client/public folder
   await middleware.installSharedStatic(app);
   await middleware.installPostGraphile(app);
+  // This proxies through to create-react-app
   await middleware.installFrontendServer(app);
 
   // And finally, we open the listen port
