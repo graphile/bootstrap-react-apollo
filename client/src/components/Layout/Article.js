81af8f8b6dccc00d3de8e46335b4a73ec2113cba
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-03-09 01:00:54 +0200

    Small layout improvements.

---
+++ client/src/components/Layout/Article.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const Article = styled.article`
+  minheight: calc(100vh - 5rem);
+`;
+
+export default Article;
---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = styled.footer`
+  padding: 0.5rem 1rem;
+  backgroundcolor: #eee;
+`;
+
+export default Footer;
