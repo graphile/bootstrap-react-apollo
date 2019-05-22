07cc66bb0dc3d9f78c22c8f5854aabf25236b877
Joe <joe.vandelay@gmail.com>
2019-05-22 16:20:38 +0200

    removes left over react client files

--- client/src/components/Layout/Article.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-const Article = styled.article`
-  min-height: calc(100vh - 5rem);
-`;
-
-export default Article;
--- client/src/components/Layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-const Footer = styled.footer`
-  padding: 0.5rem 1rem;
-  background-color: #eee;
-`;
-
-export default Footer;
