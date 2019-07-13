7d84cb688380dc402b76dfc6f2cbf3a5b359416b
David Baumgold <david@davidbaumgold.com>
2019-07-13 15:07:36 +0200

    Add reset password flow

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
 import VerifyUserEmailPage from "./components/VerifyUserEmailPage";
+import ForgotPasswordPage from "./components/ForgotPasswordPage";
+import ResetPasswordPage from "./components/ResetPasswordPage";
 import NotFoundPage from "./components/NotFoundPage";
 
 class App extends Component {
@@ -17,6 +19,8 @@ class App extends Component {
         <GraphQLRoute path="/login" exact component={LoginPage} />
         <GraphQLRoute path="/register" exact component={RegisterPage} />
         <GraphQLRoute path="/verify-email/:token" exact component={VerifyUserEmailPage} />
+        <GraphQLRoute path="/forgot-password" exact component={ForgotPasswordPage} />
+        <GraphQLRoute path="/reset-password/:userId(\d+)/:token" exact component={ResetPasswordPage} />
 
         <Route component={NotFoundPage} />
       </Switch>
