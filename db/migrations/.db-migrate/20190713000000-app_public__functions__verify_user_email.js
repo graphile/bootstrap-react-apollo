1f5eea4742e2bfe778c4945b20c30fac01b2a13c
David Baumgold <david@davidbaumgold.com>
2019-07-13 12:21:56 +0200

    Move verifyUserEmail mutation into database function

---
+++ db/migrations/.db-migrate/20190713000000-app_public__functions__verify_user_email.js
@@ -0,0 +1,2 @@
+/* This is boilerplate, you want the parent directory for the actual migrations */
+module.exports = require('../../scripts/migrate')("20190713_000000.app_public.functions.verify_user_email");
